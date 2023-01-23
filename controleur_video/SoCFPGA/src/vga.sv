1cea799a8d545f84bf6aa98c4db4b3291809debc
Alex PHIMANESONE <alexpparcoursup@gmail.com>
2023-01-23 16:59:39 +0100

    Lecture en SDRAM: simulation OK

--- controleur_video/SoCFPGA/src/vga.sv
+++ controleur_video/SoCFPGA/src/vga.sv
@@ -20,12 +20,24 @@ localparam TOTAL_HEIGHT = VFP + VPULSE + VBP + VDISP;
 localparam HBLANK       = TOTAL_WIDTH - HDISP;
 localparam VBLANK       = TOTAL_HEIGHT - VDISP;
 
+//==================================
 // Déclaration des signaux internes
+//==================================
+
+// Pour l'affichage
 
 logic [$clog2(TOTAL_HEIGHT):0] ln_cnt;
 logic [$clog2(TOTAL_WIDTH):0]  pixel_cnt;
 logic [$clog2(VDISP):0]        ln_cnt_disp;
 logic [$clog2(HDISP):0]        pixel_cnt_disp;
+
+// Pour la lecture dans la SDRAM
+
+logic [31:0] pixel_adr;
+
+//===========
+// Affichage
+//===========
 
 // Gestion de l'horloge
 
@@ -80,29 +92,57 @@ else
 
 // Generation d'une mire
 
-always_ff @(posedge pixel_clk)
-if (pixel_rst)
-    video_ifm.RGB <= {8'h0, 8'h0, 8'h0};
-else
-begin
-    video_ifm.RGB <= {8'h0, 8'h0, 8'h0};
-    if ((ln_cnt_disp[3:0] == 4'b0) || (pixel_cnt_disp[3:0] == 4'b0))
-        video_ifm.RGB <= {8'hff, 8'hff, 8'hff};
-end
+//always_ff @(posedge pixel_clk)
+//if (pixel_rst)
+//    video_ifm.RGB <= {8'h0, 8'h0, 8'h0};
+//else
+//begin
+//    video_ifm.RGB <= {8'h0, 8'h0, 8'h0};
+//    if ((ln_cnt_disp[3:0] == 4'b0) || (pixel_cnt_disp[3:0] == 4'b0))
+//        video_ifm.RGB <= {8'hff, 8'hff, 8'hff};
+//end
 
-// Maître Wishbone bidon
+//===================
+// Lecture en SDRAM
+//===================
 
-assign wshb_ifm.dat_ms = 32'hBABECAFE;
-assign wshb_ifm.adr    = '0;
 assign wshb_ifm.cyc    = 1'b1;
-assign wshb_ifm.sel    = 4'b1111;
-assign wshb_ifm.stb    = 1'b1;
-assign wshb_ifm.we     = 1'b1;
+assign wshb_ifm.we     = 1'b0;
+assign wshb_ifm.sel    = 4'b0111;
+
 assign wshb_ifm.cti    = '0;
 assign wshb_ifm.bte    = '0;
 
+//always_ff @(posedge wshb_ifm.clk)
+//if (wshb_ifm.rst)
+//    (ecrire dans la fifo) <= {8'h0, 8'h0, 8'h0};
+//else
+//    (ecrire dans la fifo) <= wshb_ifm.ack & wshb_ifm.dat_sm[23:0]; 
 
+always_ff @(posedge wshb_ifm.clk)
+if (wshb_ifm.rst)
+    pixel_adr <= 32'b0;
+else
+    if (pixel_adr >= VDISP * HDISP - 1)
+        pixel_adr <= 32'b0;
+    else
+        if (wshb_ifm.ack)
+            pixel_adr <= pixel_adr + 1;
+
+assign wshb_ifm.adr = 4 * pixel_adr;
+
+//pour l'instant on a pas le signal (fifo non pleine) donc simple
+//always_ff @(posedge wshb_ifm.clk)
+//if (wshb_ifm.rst)
+//    wshb_ifm.stb <= 1'b0;
+//else
+//    wshb_fm.stb <= (fifo non pleine)
+assign wshb_ifm.stb = 1'b1;
 
 
 endmodule
 
+/*
+compteur dep de ack, +4 que si ack
+droit de faire une requete que si la fifo n'est pas pleine
+*/
